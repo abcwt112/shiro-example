89188f69a62f82808020f215c97aa69b0aa628fa
Jet <jyzjyz12@163.com>
2015-06-20 21:28:37 +0800

    Signed-off-by: Jet <jyzjyz12@163.com>

--- shiro-example-chapter3/src/test/java/com/github/zhangkaitao/shiro/chapter3/permission/BitAndWildPermissionResolver.java
+++ shiro-example-chapter3/src/test/java/com/github/zhangkaitao/shiro/chapter3/permission/BitAndWildPermissionResolver.java
@@ -3,7 +3,6 @@ package com.github.zhangkaitao.shiro.chapter3.permission;
 import org.apache.shiro.authz.Permission;
 import org.apache.shiro.authz.permission.PermissionResolver;
 import org.apache.shiro.authz.permission.WildcardPermission;
-import org.apache.shiro.realm.text.IniRealm;
 
 /**
  * <p>User: Zhang Kaitao
@@ -12,11 +11,13 @@ import org.apache.shiro.realm.text.IniRealm;
  */
 public class BitAndWildPermissionResolver implements PermissionResolver {
 
-    
+	@Override
     public Permission resolvePermission(String permissionString) {
         if(permissionString.startsWith("+")) {
             return new BitPermission(permissionString);
         }
         return new WildcardPermission(permissionString);
     }
+	
+	
 }
